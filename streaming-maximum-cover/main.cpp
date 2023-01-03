9f1af8975b523ac133bbf326142176a40ad9cf9f
caesiumCode <caesiumcode@aol.com>
2023-01-03 15:24:54 +1100

    Initial Commit

---
+++ streaming-maximum-cover/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  streaming-maximum-cover
+//
+//  Created by Stephen Jaud on 3/1/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
